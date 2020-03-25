77f3b4bbd8e28c9bb9ba218d75796b50e223f7fa
lijuan <761916205@qq.com>
2020-03-25 11:59:54 +0800

    [add]完成home页侧边栏

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-24 14:56:32
- * @LastEditTime: 2020-03-24 19:59:30
+ * @LastEditTime: 2020-03-25 10:29:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /studyCode/vue_shop/src/main.js
@@ -16,6 +16,13 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 设置请求的根地址
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 相当于在请求之前预处理
+// 给axios请求拦截器添加token
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+})
 // 把axios赋值给Vue的原型对象上
 Vue.prototype.$http = axios
 Vue.use(axios)
--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-03-24 14:59:06
- * @LastEditTime: 2020-03-24 20:18:07
+ * @LastEditTime: 2020-03-25 09:24:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edite
  * @FilePath: /studyCode/vue_shop/src/plugins/element.js
@@ -9,13 +9,36 @@
 import Vue from 'vue'
 // 按需引入element-ui组件
 // 逗号前面不能有空格 后面必须要有空格
-import { Button, Form, FormItem, Input} from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  container,
+  header,
+  aside,
+  main,
+  menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem
+} from 'element-ui'
 // 导入弹框
-import {Message} from 'element-ui'
+import {
+  Message
+} from 'element-ui'
 // 在全局中注册这些组件 使他们可用
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
-Vue.use(Input,Message)
+Vue.use(Input, Message)
+Vue.use(container)
+Vue.use(header)
+Vue.use(aside)
+Vue.use(main)
+Vue.use(menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
 // 需要挂载 这样 只要使用this.$message就可以获取弹框提示
 Vue.prototype.$message = Message
