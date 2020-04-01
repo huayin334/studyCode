1997d91ca997bd8dd33c0af5a9c272592bb33272
lijuan <761916205@qq.com>
2020-04-01 11:00:50 +0800

    [fix]解决bug和新增删除及修改tag功能

--- vue_shop/src/components/goods/Params.vue
+++ vue_shop/src/components/goods/Params.vue
@@ -53,7 +53,26 @@
           <!-- 动态参数表格 -->
           <el-table :data="manyTableData" border stripe>
             <!-- 展开行 -->
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+              <template slot-scope="scope">
+                <el-tag v-for="(item,i) in scope.row.attr_vals" :key="i" closable @close="handleClose(scope.row,i)">
+                  {{item}}
+                </el-tag>
+                <!--输入文本框-->
+                <el-input
+                  class="input-new-tag"
+                  v-if="scope.row.inputVisible"
+                  v-model="scope.row.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(scope.row)"
+                  @blur="handleInputConfirm(scope.row)"
+                >
+                </el-input>
+                <!--添加tag按钮-->
+                <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ 新增标签</el-button>
+              </template>
+            </el-table-column>
             <!-- 索引列 -->
             <el-table-column type="index" label="#"></el-table-column>
             <el-table-column
@@ -67,12 +86,15 @@
                   icon="el-icon-edit"
                   type="primary"
                   @click="showEditDialog(scope.row.attr_id)"
-                ></el-button>
+                >编辑
+                </el-button>
                 <el-button
                   size="mini"
                   icon="el-icon-delete"
                   type="danger"
-                ></el-button>
+                  @click="showDelDialog(scope.row.attr_id)"
+                >删除
+                </el-button>
               </template>
             </el-table-column>
           </el-table>
@@ -90,7 +112,8 @@
           <!-- 静态属性表格 -->
           <el-table :data="onlyTableData" border stripe>
             <!-- 展开行 -->
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+            </el-table-column>
             <!-- 索引列 -->
             <el-table-column type="index" label="#"></el-table-column>
             <el-table-column
@@ -104,10 +127,11 @@
                   icon="el-icon-edit"
                   type="primary"
                   @click="showEditDialog(scope.row.attr_id)"
-                >删除
+                >编辑
                 </el-button
                 >
                 <el-button size="mini" icon="el-icon-delete" type="danger"
+                           @click="showDelDialog(scope.row.attr_id)"
                 >删除
                 </el-button
                 >
@@ -164,6 +188,18 @@
         <el-button type="primary" @click="editParams">确 定</el-button>
       </span>
     </el-dialog>
+    <!--删除按钮-对话框 -->
+    <el-dialog
+      title="提示"
+      :visible.sync="delDialogVisible"
+      width="30%"
+    >
+      <span>确定要删除吗？</span>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="delDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="delAttr">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -182,7 +218,7 @@
         //级联选择器双向绑定的数据
         selectedCateKeys: [],
         //被激活的页签名称
-        activeName: "only",
+        activeName: "many",
         //动态属性的数据
         manyTableData: [],
         //静态属性的数据
@@ -214,7 +250,12 @@
               trigger: "blur"
             }
           ]
-        }
+        },
+        delDialogVisible: false,
+        delAttrId: "",
+        inputVisible: false,
+        //文本框输入的内容
+        inputValue: ""
       };
     },
     created() {
@@ -243,6 +284,9 @@
         // 只允许选择三级分类
         if (this.selectedCateKeys.length !== 3) {
           this.selectedCateKeys = [];
+          //把下面的表格也清空
+          this.manyTableData = [];
+          this.onlyTableData = []
         } else {
           // 根据所选id和当前所处的面板，获取对应的参数
           const {data: res} = await this.$http.get(
@@ -252,10 +296,21 @@
             }
           );
           if (res.meta.status !== 200) return this.$message.error("获取失败");
+          // console.log(res.data);
           this.$message.success("获取成功");
-          // console.log(res.data)
-          // console.log(this.activeName)
+          res.data.forEach(item => {
+            //用空格分割字符串 转换为数组
+            //判断是否为空 若为空直接返回空数组
+            item.attr_vals = item.attr_vals ? item.attr_vals.split(",") : [];
+            //  循环的时候顺便给data绑定inputVisible和inputValue
+            //  这样每一行的tag新增标签都是一个独特的tag标签，不会互相影响
+            //  控制文本框的显示与隐藏
+            item.inputVisible = false;
+            //  控制文本框中输入的值
+            item.inputValue = "";
 
+          });
+          console.log(res.data);
           if (this.activeName === "many") {
             this.manyTableData = res.data;
             //   console.log(this.manyTableData)
@@ -293,7 +348,6 @@
           this.addDialogVisible = false;
         });
       },
-
       // 编辑动态或者静态参数
       async showEditDialog(attr_id) {
         this.editDialogVisible = true;
@@ -330,10 +384,78 @@
           this.$message.success("更新成功");
           // 刷新表格
           this.getParamsData();
-          this.editDialogVisible = true;
+          //关闭对话框
+          this.editDialogVisible = false;
         });
+      },
+      //删除按钮
+      showDelDialog(delAttrId) {
+        this.delAttrId = delAttrId;
+        this.delDialogVisible = true;
+      },
+      //确定删除
+      async delAttr() {
+        const {data: res} = await this.$http.delete(`categories/${this.cateId}/attributes/${this.delAttrId}`);
+        if (res.meta.status !== 200) return this.$message.error("删除失败");
+        this.$message.success("删除成功");
+        this.delDialogVisible = false;
+        this.getParamsData();
+        this.delAttrId = "";
+      },
+      //控制新添加tag的input框和btn切换
+      showInput(row) {
+        row.inputVisible = true;
+        //让文本框自动获取焦点
+        //$nextTick当页面的元素被重新渲染时，才会调用回调函数
+        //因为input被渲染出来的时候，才能获取到input元素
+        this.$nextTick(() => {
+          this.$refs.saveTagInput.$refs.input.focus();
+        });
+      },
+      //文本框失去了焦点或者按下enter都会触发
+      async handleInputConfirm(row) {
+        //去除头尾空格 trim不会改变原字符串
+        let inputValue = row.inputValue.trim();
+        if (inputValue.length === 0) {
+          row.inputVisible = false;
+          row.inputValue = "";
+          return 0;
+        }
+        /*
+        * 输入的内容需要做后续处理
+        * 将输入的值加入到attr_vals数组中
+        * */
+        row.attr_vals.push(inputValue);
+        //关闭input，并且input的值设为空
+        row.inputValue = "";
+        row.inputVisible = false;
+        this.saveAttrVals(row);
+      },
+      async saveAttrVals(row) {
+        //发起网络请求，保存这次操作
+        const {data: res} = await this.$http.put(`categories/${this.cateId}/attributes/${row.attr_id}`, {
+          attr_name: row.attr_name,
+          attr_sel: row.attr_sel,
+          //将数组转为字符串
+          attr_vals: row.attr_vals.join(",")
+        });
+        if (res.meta.status !== 200) {
+          return this.$message.error("修改失败");
+        } else {
+          this.$message.success("修改成功");
+        }
+      },
+      //关闭tag标签,删除对应的参数
+      handleClose(row, i) {
+        console.log(row);
+        row.attr_vals.splice(i, 1);
+        // this.getParamsData();
+        // this.$message.success("删除成功");
+        this.saveAttrVals(row);
       }
     },
+
+
     //计算属性
     computed: {
       //如果按钮需要被禁用 返回true
@@ -343,7 +465,8 @@
         } else {
           return false;
         }
-      },
+      }
+      ,
       //   当前选中的三级分类的id
       cateId() {
         if (this.selectedCateKeys.length === 3) {
@@ -351,7 +474,8 @@
         } else {
           return null;
         }
-      },
+      }
+      ,
       // 动态计算标题的文本
       titleText() {
         if (this.activeName === "many") {
@@ -361,7 +485,8 @@
         }
       }
     }
-  };
+  }
+  ;
 </script>
 
 <style lang="less" scoped>
@@ -376,5 +501,14 @@
 
   .el-table {
     margin-top: 15px;
+  }
+
+  .el-tag {
+    margin: 5px;
+  }
+
+  /*控制输入框的长度*/
+  .input-new-tag {
+    width: 120px;
   }
 </style>
